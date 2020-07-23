2489afe6136ed9d02f330857b9567c77eb3c9c04
8Kate <katharina.schmid@hs-furtwangen.de>
2020-07-23 20:23:39 +0200

    fixed port for heroku

--- server/App.ts
+++ server/App.ts
@@ -1,9 +1,11 @@
 import * as Http from "http";
 import * as Url from "url";
 
+let port = process.env.PORT || 8080;
+
 export namespace App {
     Http.createServer(function (req, res) {
         res.write('Hello World!'); //write a response to the client
         res.end(); //end the response
-    }).listen(8080);
+    }).listen(port);
 }
