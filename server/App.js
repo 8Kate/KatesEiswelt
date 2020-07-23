d1f8c113eed1a0b4adf49ba6f016e77dfc25478d
8Kate <katharina.schmid@hs-furtwangen.de>
2020-07-23 20:32:09 +0200

    fixed port again

--- server/App.js
+++ server/App.js
@@ -2,10 +2,11 @@
 exports.__esModule = true;
 exports.App = void 0;
 var Http = require("http");
+var port = process.env.PORT || 8080;
 var App;
 (function (App) {
     Http.createServer(function (req, res) {
         res.write('Hello World!'); //write a response to the client
         res.end(); //end the response
-    }).listen(8080);
+    }).listen(port);
 })(App = exports.App || (exports.App = {}));
